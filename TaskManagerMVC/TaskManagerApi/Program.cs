f9bc9370518d237a14bd1ba8278b424c238b829d
RASHMI GUPTA <61780301+Rashmi24041997@users.noreply.github.com>
2024-10-30 12:03:38 +0530

    tried to call apis from angular

--- TaskManagerMVC/TaskManagerApi/Program.cs
+++ TaskManagerMVC/TaskManagerApi/Program.cs
@@ -3,48 +3,43 @@ using MvcTaskManager.Models;
 
 namespace TaskManagerApi
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+      var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+      // Add services to the container.
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddDbContext<TaskManagerDbContext>();
-            // Add CORS configuration
-            builder.Services.AddCors(options =>
-            {
-                options.AddPolicy("AllowSpecificOrigin",
-                    builder => builder
-                    .WithOrigins("http://localhost:4200")
-                        .AllowAnyOrigin() // Angular app URL
-                        .AllowAnyHeader()
-                        .AllowAnyMethod()
-                        .AllowCredentials());
-            });
+      builder.Services.AddControllers();
+      // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+      builder.Services.AddEndpointsApiExplorer();
+      builder.Services.AddSwaggerGen();
+      builder.Services.AddDbContext<TaskManagerDbContext>();
+      // Add CORS configuration
+      builder.Services.AddCors(options =>
+      {
+        options.AddPolicy("AllowAll",
+                  builder => builder
+                      .AllowAnyOrigin()
+                      .AllowAnyHeader()
+                      .AllowAnyMethod());
+      });
 
-            var app = builder.Build();
+      var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+      // Configure the HTTP request pipeline.
+      if (app.Environment.IsDevelopment())
+      {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+      }
+      app.UseCors("AllowAll"); // CORS must come before any middleware like Authorization
 
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
+      app.UseHttpsRedirection();
+      app.UseRouting();
+      app.UseAuthorization();
+      app.MapControllers();
     }
+  }
 }
